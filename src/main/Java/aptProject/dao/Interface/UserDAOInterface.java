6c2bec6e8c2249d8b01c336d913d6bd5c3aaa59f
Subarna Shrestha <subarna.shrestha.a24@icp.edu.np>
2026-05-17 11:57:41 +0545

    update DAO interfaces and implementation

--- src/main/Java/aptProject/dao/Interface/UserDAOInterface.java
+++ src/main/Java/aptProject/dao/Interface/UserDAOInterface.java
@@ -3,6 +3,7 @@ package aptProject.dao.Interface;
 import aptProject.model.User;
 
 public interface UserDAOInterface {
+
     boolean register(User user);
 
     User findByEmail(String email);
@@ -11,12 +12,9 @@ public interface UserDAOInterface {
 
     User login(String usernameOrEmail, String password);
 
-    /** Update full_name and email for a user */
     boolean updateProfile(int userId, String fullName, String email);
 
-    /** Update password_hash for a user */
     boolean updatePassword(int userId, String newHashedPassword);
 
-    /** Update profile_image path for a user */
     boolean updateProfileImage(int userId, String imagePath);
 }
